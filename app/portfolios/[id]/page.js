72d265df1ca8d9cb980f36ffd2f0d52e0a632d9d
RnRukon <rukon.js@gmail.com>
2024-01-11 22:20:53 +0600

    update header content

--- app/portfolios/[id]/page.js
+++ app/portfolios/[id]/page.js
@@ -5,7 +5,7 @@ import Navigation from "@/components/navigation/navigation";
 const Page = ({params}) => {
     const portfolio= portfolioData?.portfolios?.future?.find(data=>data?.id===params?.id);
     return (
-        <div>
+        <div className="text-white bg-gradient-to-tl to-[#060212] via-[#021a30] from-[#410b42]">
             <Navigation/>
            <div className="max-w-[1200px] mx-auto py-28 px-5">
                <Portfolio data={portfolio}/>

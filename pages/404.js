802d071087d22c3bf11650c61f452e9249c94c38
rukonpro <rukon.js@gmail.com>
2024-01-21 00:35:08 +0600

    404 page add

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotfoundPages= () => {
+    return (
+        <div className="w-screen h-screen flex justify-center items-center text-red-500">
+            <h1>404</h1>
+        </div>
+    );
+};
+
+export default NotfoundPages;

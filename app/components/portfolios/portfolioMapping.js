aadd3b8b56eac1d0501d047a49379467851f4085
rukonpro <rukon.js@gmail.com>
2024-01-24 20:11:04 +0600

    portfolio animation card

--- app/components/portfolios/portfolioMapping.js
+++ app/components/portfolios/portfolioMapping.js
@@ -8,30 +8,25 @@ const PortfolioMapping = () => {
             <ol className="grid grid-cols-1 md:grid-cols-2 items-center  gap-5  pb-10">
                 {portfolioData?.portfolios?.future.map((data) =>{
                     return (
-                       <li key={data?.id}>
-                                <Link href={`/portfolios/${data?.id}`}
-                                >
-                                    <div
-                                        data-aos="fade-up"
-                                        data-aos-anchor-placement="center-bottom"
-                                        data-aos-duration="1000"
-                                        className=" bg-gradient-to-r from-[#0d1247]/30 to-gray-900/30 hover:bg-[#280f5f] h-full flex items-center py-[16px] px-[32px] font-[400] rounded-2xl "
-                                    >
-
+                       <li key={data?.id}
+                           data-aos="fade-up"
+                           data-aos-anchor-placement="center-bottom"
+                           data-aos-duration="1000"
+                           className="bg-gradient-to-r from-[#0d1247]/30 to-gray-900/30 hover:bg-[#280f5f] h-full flex items-center py-[16px] px-[32px] font-[400] rounded-2xl portfolioCardContainer">
+                                <Link href={`/portfolios/${data?.id}`}>
                                         <div className="grid grid-cols-3 gap-5 items-center">
 
                                             <div className="col-span-1">
                                                 <h1 className="text-[24px] text-white lg:text-[40px]">{data?.title.split(" ")[0].slice(0, 30)}</h1>
                                             </div>
-                                            <div className="col-span-2">
+                                            <div className="col-span-2 portfolioCardImageContainer">
                                                 <Image
-                                                    className=" w-full cursor-pointer  rounded-2xl hover:transform  transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-x-100  duration-300"
+                                                    className=" w-full cursor-pointer  rounded-2xl   portfolioCardAnimation"
                                                     src={data?.images?.[0]}
                                                     alt="protfoliocover"
                                                 />
                                             </div>
                                         </div>
-                                    </div>
                                 </Link>
                             </li>
                     )

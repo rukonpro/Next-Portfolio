8a68e66b2ef7468a3c30b5b05b526f3fdc34a919
rukonpro <rukon.js@gmail.com>
2024-04-07 15:36:24 +0600

    blog description add

--- app/components/blogs/blogs.js
+++ app/components/blogs/blogs.js
@@ -19,54 +19,56 @@ const Blogs = () => {
                             className="text-white z-10 md:text-5xl text-2xl font-bold  absolute tracking-[4px]">Blogs</h1>
                     </div>
 
-                    <desc className=" mx-auto text-justify pt-10">
-                        <blockquote
-                            data-aos="fade-up"
-                            data-aos-anchor-placement="bottom-bottom"
-                            data-aos-duration="1000"
-                            className="  font-bold  text-base text-slate-400 tracking-[2px]  pt-5 ">
-                            {`Welcome to my portfolio blog where I delve into the dynamic world of MERN (MongoDB,
+                    <div className="pt-16">
+                        <desc className=" mx-auto text-justify ">
+                            <blockquote
+                                data-aos="fade-up"
+                                data-aos-anchor-placement="bottom-bottom"
+                                data-aos-duration="1000"
+                                className="  font-bold  text-base text-slate-400 tracking-[2px]  pt-5 ">
+                                {`Welcome to my portfolio blog where I delve into the dynamic world of MERN (MongoDB,
                             Express.js,
                             React.js, Node.js) stack development! As a passionate Full Stack Developer, I am committed
                             to
                             creating innovative and seamless web experiences. Here, I document my journey, insights, and
                             the
                             challenges I encounter while architecting robust applications using the MERN stack.`}
-                        </blockquote>
-                        <blockquote
-                            data-aos="fade-up"
-                            data-aos-anchor-placement="bottom-bottom"
-                            data-aos-duration="1000"
-                            className="font-bold  text-base text-slate-400 tracking-[2px]  pt-5">
-                            {`Discover how I leverage MongoDB's flexibility for database management, harness the power of
+                            </blockquote>
+                            <blockquote
+                                data-aos="fade-up"
+                                data-aos-anchor-placement="bottom-bottom"
+                                data-aos-duration="1000"
+                                className="font-bold  text-base text-slate-400 tracking-[2px]  pt-5">
+                                {`Discover how I leverage MongoDB's flexibility for database management, harness the power of
                             Express.js for building scalable APIs, and utilize React.js to craft interactive user
                             interfaces. Follow along as I showcase my proficiency in Node.js, enabling server-side
                             scripting
                             and handling asynchronous operations effortlessly.`}
-                        </blockquote>
-                        <blockquote
-                            data-aos="fade-up"
-                            data-aos-anchor-placement="bottom-bottom"
-                            data-aos-duration="1000"
-                            className="font-bold  text-base text-slate-400 tracking-[2px]  pt-5">
-                            {`From tackling complex application logic to optimizing performance, my portfolio blog offers
+                            </blockquote>
+                            <blockquote
+                                data-aos="fade-up"
+                                data-aos-anchor-placement="bottom-bottom"
+                                data-aos-duration="1000"
+                                className="font-bold  text-base text-slate-400 tracking-[2px]  pt-5">
+                                {`From tackling complex application logic to optimizing performance, my portfolio blog offers
                             a
                             glimpse into my expertise in developing modern web solutions. Whether you're a fellow
                             developer
                             seeking inspiration or a business owner looking to elevate your online presence, explore my
                             projects and learn how MERN stack development can elevate your digital footprint.`}
-                        </blockquote>
-                        <blockquote
-                            data-aos="fade-up"
-                            data-aos-anchor-placement="bottom-bottom"
-                            data-aos-duration="1000"
-                            className=" font-bold  text-base text-slate-400 tracking-[2px]  pt-5">
-                            {` Join me on this exhilarating journey of innovation and continuous learning as I push the
+                            </blockquote>
+                            <blockquote
+                                data-aos="fade-up"
+                                data-aos-anchor-placement="bottom-bottom"
+                                data-aos-duration="1000"
+                                className=" font-bold  text-base text-slate-400 tracking-[2px]  pt-5">
+                                {` Join me on this exhilarating journey of innovation and continuous learning as I push the
                             boundaries of what's possible in the realm of Full Stack and React development. Let's embark
                             together on the quest to build exceptional web experiences that captivate and engage users
                             worldwide.`}
-                        </blockquote>
-                    </desc>
+                            </blockquote>
+                        </desc>
+                    </div>
                     <div
                         className="grid xl:grid-cols-4 lg:grid-cols-3  sm:grid-cols-2 grid-cols-1 gap-4 gap-y-16  pt-28 text-white">
                         {

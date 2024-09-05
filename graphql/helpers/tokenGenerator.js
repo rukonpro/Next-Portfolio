6383951ffda633dd248072d86bd4f83976b1a8c6
rukonpro <rukon.js@gmail.com>
2024-09-05 17:20:46 +0600

    remove graphql

--- graphql/helpers/tokenGenerator.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const tokenGenerator = (options) => {
-    return jwt.sign(options, process.env.JWT_SECRET_KEY, { expiresIn: '30d' });
-}
-
-export default tokenGenerator;

c62d4683959d573ee1b3c7413b2338db19afd4a9
rukonpro <rukon.js@gmail.com>
2024-10-12 15:57:57 +0600

    vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,13 @@
-"use client"
 import './globals.css'
 import 'aos/dist/aos.css';
 import 'react-quill/dist/quill.snow.css';
 import { chewy } from "@/app/utils/fonts/fonts";
-import { SessionProvider } from 'next-auth/react';
 import { Analytics } from "@vercel/analytics/react"
-// export const metadata = {
-//   title: 'MERN Stack Dev. - Rukon.Pro',
-//   description: 'Explore the portfolio of Your Name, a skilled MERN stack developer with expertise in building robust web applications.',
-// }
+import AppSessionProvider from './Context/AppSessionProvider';
+export const metadata = {
+  title: 'MERN Stack Dev. - Rukon.Pro',
+  description: 'Explore the portfolio of Your Name, a skilled MERN stack developer with expertise in building robust web applications.',
+}
 
 export default function RootLayout({ children }) {
   const isDev = process.env.NODE_ENV === 'development';
@@ -18,9 +17,9 @@ export default function RootLayout({ children }) {
     <html lang="en" className="scroll-smooth" suppressHydrationWarning={isDev}>
       <body className={chewy.className} >
         <main>
-          <SessionProvider>
+          <AppSessionProvider>
             {children}
-          </SessionProvider>
+          </AppSessionProvider>
           <Analytics />
         </main>
       </body>

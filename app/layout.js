be0364437ed35fb22f5d18fc0a0198f0338c2c88
rukonahmed <rukon.ahmed@gmail.com>
2025-11-13 15:51:13 +0600

    feat(analytics): integrate vercel speed insights
    Adds the @vercel/speed-insights package to the project and integrates the SpeedInsights component into the root layout, enabling performance monitoring.

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "aos/dist/aos.css";
 import "react-quill/dist/quill.snow.css";
 import {inter} from "@/app/utils/fonts/fonts";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import AppSessionProvider from "./Context/AppSessionProvider";
 import CustomCursor from "@/app/components/ui/customCursor/CustomCursor";
 export const metadata = {
@@ -43,6 +44,7 @@ export default function RootLayout({ children }) {
             {children}
           </AppSessionProvider>
           <Analytics />
+          <SpeedInsights/>
         </main>
       </body>
     </html>

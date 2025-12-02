812db916abf7e1dc2af9d9b8c851664dfa811d20
rukonahmed <rukon.ahmed@gmail.com>
2025-12-02 19:30:09 +0600

    feat: Initialize AOS library in Navigation component

--- app/components/layout/navigation/navigation.js
+++ app/components/layout/navigation/navigation.js
@@ -1,13 +1,17 @@
 "use client";
 
-import React from "react";
+import React, {useEffect} from "react";
 import Link from "next/link";
 import Image from "next/image";
 import DesktopMenu from "./DesktopMenu";
 import Logo from "@/public/images/rukonpro_logo.png";
-
+import AocFuc from "@/app/utils/AOS";
+import "aos/dist/aos.css";
 
 const Navigation =() => {
+    useEffect(() => {
+        AocFuc();
+    }, []);
     return (
        <React.Fragment>
            <nav className="fixed top-0 left-0 z-50 w-full backdrop-blur-3xl bg-transparent">
